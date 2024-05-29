fb0462c4c8f2f739da23f7cb1845ad36b2a84b74
Chris Cooley <ccooley@bonadmin.com>
2024-05-29 10:55:16 -0600

    Add filters

--- script.js
+++ script.js
@@ -88,3 +88,62 @@ const map3 = () => {
         console.log(`Original: ${numbers[i]}, changed: ${changed[i]}`);
     }
 }
+
+const testNumberFilter = () => {
+    const nums = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const filteredNumbers = numberFilter(nums);
+    console.log(filteredNumbers);
+};
+
+const numberFilter = (numbers) => {
+    const result = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= 0 && numbers[i] <=5) {
+            result.push(numbers[i]);
+        }
+    }
+    return result;
+};
+
+const filter1 = () => {
+    const numberListStr = prompt("Enter a list of numbers");
+    const odds = [];
+    const numArray = numberListStr.split(",");
+    for (let i = 0; i < numArray.length; i++) {
+        const num = Number(numArray[i]);
+        if (num % 2 !== 0) {
+            odds.push(num);
+        } 
+    }
+
+    for (let i = 0; i < odds.length; i++) {
+        console.log(odds[i]);
+    }
+}
+
+const filterModifyArray = () => {
+    const randomArray = [0, 2, 76, 8, 2, 1, 5, 2, 6, 9, 9, 11, 0, 15, 2, 8, 9];
+    filter2(randomArray);
+
+    console.log(randomArray);
+};
+
+const filter2 = (numbersToFilter) => {
+    let i = 0;
+    while (i < numbersToFilter.length) {
+        if (numbersToFilter[i] < 8 || numbersToFilter[i] > 15) {
+            numbersToFilter.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+};
+
+const filter3 = () => {
+    const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    const odds = numArray.filter((number) => number % 2 !== 0);
+
+    console.log(odds);
+};
